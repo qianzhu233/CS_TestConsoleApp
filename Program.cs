37b0e8f01492172f26cb7cc55e78a60d90543739
qianzhu233 <qianzhu233@outlook.com>
2023-07-17 21:41:50 +0800

    test1.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Std
+{
+	class Program
+	{
+		static void Main()
+		{
+            var account=new BankAccount("qianzhu233",114514);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+	}
+}

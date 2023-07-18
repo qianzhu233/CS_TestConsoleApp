ccdf4315b9d13e520c932884690d9c761d7deb64
qianzhu233 <qianzhu233@outlook.com>
2023-07-18 14:40:06 +0800

    push_test

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Std
+{
+	class Program
+	{
+		static void Main()
+		{
+            Console.WriteLine("Your name ...");
+            string a = Console.ReadLine();
+            Console.WriteLine("Hello, world. ",a,"!");
+        }
+	}
+}
